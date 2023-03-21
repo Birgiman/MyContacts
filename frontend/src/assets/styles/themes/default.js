bda4b7461ae1bd079413155df241a462a6c3f74f
Birgiman <eduardo.birgiman@outlook.com>
2023-03-20 21:52:55 -0300

    Making Contacts List

--- frontend/src/assets/styles/themes/default.js
+++ frontend/src/assets/styles/themes/default.js
@@ -1,3 +1,16 @@
 export default {
-  backgroundColor: '#F6F5FC',
+
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0',
+    },
+    gray: {
+      900: '#222222',
+      200: '#bcbcbc',
+    },
+  },
 };
