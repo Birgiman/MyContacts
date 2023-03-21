65b9c402a89c2c1bc2dbce4d19b56f1a8fd947fb
Birgiman <eduardo.birgiman@outlook.com>
2023-03-21 17:46:30 -0300

    Making Button component

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -16,6 +17,8 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">Salvar Alterações</Button>
+      <Button type="button" disabled>Salvar Alterações</Button>
     </>
 
   );
