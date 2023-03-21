dd10a5dc788b9bfbd2acc469572c5c2e28b22a39
Birgiman <eduardo.birgiman@outlook.com>
2023-03-20 22:10:03 -0300

    Config Router.js

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
+import { Container } from './styles';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
 
-import { Container } from './styles';
+import Routes from '../../Routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
