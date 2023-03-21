3fb80a34e5d1fa1b5b5e6fddfbb2c0efe3705dcc
Birgiman <eduardo.birgiman@outlook.com>
2023-03-20 22:32:13 -0300

    Refactoring Home Page and Header Component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,3 +1,91 @@
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Home Page';
+  return (
+    <Container>
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Birgiman</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo.birgiman@outlook.com</span>
+            <span>(51) 12345-6789</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Birgiman</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo.birgiman@outlook.com</span>
+            <span>(51) 12345-6789</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Eduardo Birgiman</strong>
+              <small>instagram</small>
+            </div>
+            <span>eduardo.birgiman@outlook.com</span>
+            <span>(51) 12345-6789</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+      </ListContainer>
+    </Container>
+  );
 }
