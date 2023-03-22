b9fc5abb03306025305bd5669be935186d7e9020
Birgiman <eduardo.birgiman@outlook.com>
2023-03-21 22:39:59 -0300

    Making Modal and refactoring Button component

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
+  padding: 0 16px;
   border: none;
   background: ${({ theme }) => theme.colors.primary.main};
   font-size: 16px;
@@ -24,4 +24,16 @@ export default styled.button`
     background: #ccc;
     cursor: default;
   }
+
+  ${({ theme, danger }) => danger && css`
+    background: ${theme.colors.danger.main};
+
+    &:hover {
+    background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
 `;
