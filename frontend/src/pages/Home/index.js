b05c40e269577158fa2e79fa4c0b5116c4edfbd7
Birgiman <eduardo.birgiman@outlook.com>
2023-03-22 15:19:40 -0300

    Making Error Status to FormGroup and Input

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -7,16 +7,10 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Loader from '../../components/Loader';
-// import Modal from '../../components/Modal';
 
 export default function Home() {
   return (
     <Container>
-
-      <Loader />
-
-      {/* <Modal danger /> */}
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato" />
