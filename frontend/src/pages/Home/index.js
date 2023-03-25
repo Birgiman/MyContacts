03a4c13319e53f88e0fc337364b2217ab0402aef
Birgiman <eduardo.birgiman@outlook.com>
2023-03-25 14:05:02 -0300

    Adding Loader State

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -9,23 +9,34 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
+import Loader from '../../components/Loader';
+import delay from '../../utils/delay';
+
 export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const filteredContacts = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (res) => {
+        await delay();
+
         const json = await res.json();
         setContacts(json);
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [orderBy]);
 
@@ -41,6 +52,8 @@ export default function Home() {
 
   return (
     <Container>
+
+      <Loader isLoading={isLoading} />
 
       <InputSearchContainer>
         <input
