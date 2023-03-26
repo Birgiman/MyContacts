be0a494ef9120f771017830ba8588983c569e054
Birgiman <eduardo.birgiman@outlook.com>
2023-03-25 22:12:52 -0300

    Making HttpClient and ContactsService to center baseURL

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -10,7 +10,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -27,11 +27,9 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
-        await delay();
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        const json = await response.json();
-        setContacts(json);
+        setContacts(contactsList);
       } catch (error) {
         console.log(error);
       } finally {
