e5fd38803d7634cb06412f915ac1120df7f9497f
Birgiman <eduardo.birgiman@outlook.com>
2023-03-29 17:35:09 -0300

    Refactoring Loader component to Spinner component

--- frontend/src/components/FormGroup/index.js
+++ frontend/src/components/FormGroup/index.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 
+import Spinner from '../Spinner';
+
 import { Container } from './styles';
 
 export default function FormGroup({ children, error, isLoading }) {
@@ -7,7 +9,11 @@ export default function FormGroup({ children, error, isLoading }) {
     <Container>
       <div className="form-item">
         { children }
-        {isLoading && <div className="loader" />}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small>{error}</small>}
     </Container>
