ee6b888dd3a7f40993467596a948cada8a0d3f76
Birgiman <eduardo.birgiman@outlook.com>
2023-03-29 15:30:12 -0300

    Making screen for any contacts created

--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -27,7 +27,7 @@ export const Header = styled.header`
 
   display: flex;
   align-items: center;
-  justify-content: ${({ hasError }) => (hasError ? 'flex-end' : 'space-between')};
+  justify-content: ${({ justifyContent }) => justifyContent};
   margin-top: 32px;
   border-bottom: 2px solid ${({ theme }) => theme.colors.gray[100]};
   padding-bottom: 16px;
@@ -142,6 +142,23 @@ export const ErrorContainer = styled.div`
 
     button {
       border: none;
+    }
+  }
+`;
+
+export const EmptyListContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    color: ${({ theme }) => theme.colors.gray[200]};
+    text-align: center;
+    margin-top: 8px;
+
+    strong {
+      color: ${({ theme }) => theme.colors.primary.main};
     }
   }
 `;
