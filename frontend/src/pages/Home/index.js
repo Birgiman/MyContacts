ffbd8d733894b437c3e5bdc6d9ff7655fbe77bbf
Birgiman <eduardo.birgiman@outlook.com>
2023-03-29 15:46:42 -0300

    Making screen for user not found

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -6,7 +6,8 @@ import {
 } from 'react';
 
 import {
-  Container, Header, ListHeader, Card, InputSearchContainer, ErrorContainer, EmptyListContainer,
+  Container, Header, ListHeader, Card, InputSearchContainer, ErrorContainer,
+  EmptyListContainer, SearchNotFoundContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -14,6 +15,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -115,6 +117,15 @@ export default function Home() {
                 à cima para cadastrar o seu primeiro!
               </p>
             </EmptyListContainer>
+          )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+              <span>
+                Nenhum resultado foi encontrado para <strong>{searchTerm}</strong>
+              </span>
+            </SearchNotFoundContainer>
           )}
 
           {filteredContacts.length > 0 && (
