3c3a9a700ad698f4ba2a03ab4cd4e5c52523bdcd
Birgiman <eduardo.birgiman@outlook.com>
2023-03-30 22:59:08 -0300

    Making Toast component to send messages to the user

---
+++ frontend/src/components/Toast/ToastMessage/styles.js
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`background: ${({ theme }) => theme.colors.primary.main};`,
+  success: css`background: ${({ theme }) => theme.colors.success.main};`,
+  danger: css`background: ${({ theme }) => theme.colors.danger.main};`,
+};
+
+export const Container = styled.div`
+  padding: 16px 32px;
+  background: ${({ theme }) => theme.colors.primary.main};
+  color: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 20px 20px -16px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${({ type }) => containerVariants[type] || containerVariants.default}
+
+  img {
+    margin-right: 8px;
+  }
+
+  & + & {
+    margin-top: 12px;
+  }
+`;
