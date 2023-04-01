28c1ecaf3d1de780b5fd9935668c8f6193787be0
Birgiman <eduardo.birgiman@outlook.com>
2023-04-01 00:14:51 -0300

    Finishing Edit Contact page

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import {
+  useState, useEffect, forwardRef, useImperativeHandle,
+} from 'react';
 
 import isEmailValid from '../../utils/isEmailValid';
 import formatPhone from '../../utils/formatPhone';
@@ -13,7 +15,7 @@ import Button from '../Button';
 
 import { ButtonContainer, Form } from './styles';
 
-export default function ContactForm({ buttonLabel, onSubmit }) {
+const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -27,6 +29,15 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
   } = useErrors();
 
   const isFormValid = (name && errors.length === 0);
+
+  useImperativeHandle(ref, () => ({
+    setFieldsValues: (contact) => {
+      setName(contact.name ?? '');
+      setEmail(contact.email ?? '');
+      setPhone(formatPhone(contact.phone ?? ''));
+      setCategoryId(contact.category_id ?? '');
+    },
+  }), []);
 
   useEffect(() => {
     async function loadCategories() {
@@ -144,9 +155,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
 
     </Form>
   );
-}
+});
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,14 +1,83 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import Loader from '../../components/Loader';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(
+          id,
+        );
+
+        contactFormRef.current.setFieldsValues(contact);
+
+        setIsLoading(false);
+        setContactName(contact.name);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+    loadContact();
+  }, [id, history]);
+
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      await ContactsService.updateContact(id, contact);
+
+      setContactName(contact.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+        duration: 3000,
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+        duration: 7000,
+      });
+    }
+  }
+
   return (
     <>
+
+      <Loader isLoading={isLoading} />
+
       <PageHeader
-        title="Editar Eduardo Birgiman"
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
       />
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
 
