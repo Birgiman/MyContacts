9e252b873f492c27945da3409a2acad43bd097a3
Birgiman <eduardo.birgiman@outlook.com>
2023-04-01 00:21:16 -0300

    Refactoring Contact Formr to add  restFileds

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,9 +1,12 @@
+import { useRef } from 'react';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
+
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -14,6 +17,8 @@ export default function NewContact() {
       };
 
       await ContactsService.createContact(contact);
+
+      contactFormRef.current.resetFields();
 
       toast({
         type: 'success',
@@ -34,6 +39,7 @@ export default function NewContact() {
         title="Novo contato"
       />
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Cadastrar"
         onSubmit={handleSubmit}
       />
