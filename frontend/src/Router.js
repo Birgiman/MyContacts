db1f55b4b23d92f9a466bd928d80547931f17735
Birgiman <eduardo.birgiman@outlook.com>
2023-04-16 12:31:09 -0300

    Replacing Route and Routes by useRoutes

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,15 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NewContact from './pages/NewContact';
 import EditContact from './pages/EditContact';
 
 export default function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewContact />} />
-      <Route path="/edit/:id" element={<EditContact />} />
-    </Routes>
-  );
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/new',
+      element: <NewContact />,
+    },
+    {
+      path: '/edit/:id',
+      element: <EditContact />,
+    },
+  ]);
+
+  return routes;
 }
